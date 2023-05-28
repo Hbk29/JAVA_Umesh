e4a1fee976595ae4119df3fded0c5412c494ad37
Hbk29 <129576120+Hbk29@users.noreply.github.com>
2023-05-28 10:10:20 +0530

    Update javaclass.java
    keep in good manner

--- javaclass.java
+++ javaclass.java
@@ -2,10 +2,11 @@ package java2020;
 
 public class javaclass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("Umesh");
 	
-}
+        }
 	
 }
 }
